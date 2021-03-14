f0b52fba94f078ef2d1c89a802167265cee96167
erkanuney <uneykn@gmail.com>
2021-03-14 13:30:53 -0500

    New File Added

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,8 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Techtorial");
 
-        System.out.println("fmnklvgit ");
-        System.out.println("erkan code");
+        System.out.println("Techtorial1");
     }
 
 
