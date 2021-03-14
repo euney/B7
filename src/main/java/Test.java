a28562e7394b2e1d09f273313d0bcd9b8cf80a60
erkanuney <uneykn@gmail.com>
2021-03-14 12:59:08 -0500

    first commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,7 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Techtorial");
 
-        System.out.println("fmnklv");
+        System.out.println("fmnklvgit ");
     }
 
 
